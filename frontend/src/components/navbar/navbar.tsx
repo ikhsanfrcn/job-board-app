fa6b56297f9500aca0bc9a4a303ff15be3cec750
Ferdy <ferdydjasni@gmail.com>
2025-05-26 15:32:06 +0700

    fix search query to job page

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import { CgCloseR, CgMenu, CgSearch } from "react-icons/cg";
 import MenuDesktop from "./menud";
 import MenuMobile from "./menum";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import MiddleMenu from "./middlemenu";
 import { IoLogInOutline } from "react-icons/io5";
 import MiddleMenuMobile from "./middlemenum";
@@ -17,7 +17,8 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenSearch, setIsOpenSearch] = useState(false);
   const toggleMenuSearch = () => setIsOpenSearch(!isOpenSearch);
-  const [searchQuery, setSearchQuery] = useState("");
+  const searchParams = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState(searchParams.get("title") || "");
   const toggleMenu = () => setIsOpen(!isOpen);
   const { data: session } = useSession();
   const isLogin = !!session;
