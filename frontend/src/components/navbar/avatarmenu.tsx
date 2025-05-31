71cfe50cbe6682a8494f8f648a8f42b35afbe931
Ferdy <ferdydjasni@gmail.com>
2025-05-31 12:21:25 +0700

    fix avatar menu when company login and fix footbar link for employer center

--- frontend/src/components/navbar/avatarmenu.tsx
+++ frontend/src/components/navbar/avatarmenu.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 export default function AvatarMenu() {
   const { data: session } = useSession();
+  const userRole = session?.user.role;
 
   return (
     <div className="font-sans">
@@ -12,12 +13,21 @@ export default function AvatarMenu() {
         <span className="text-green-600">{session?.user.username}</span>
       </div>
       <hr className="text-gray-300 pb-2" />
+      {userRole === "company" ? (
+
+      <Link
+        href={"/company/manage-jobs"}
+        className="block w-full py-2 px-2 mb-2 text-sm text-gray-800 hover:bg-gray-200 transition duration-300"
+      >
+        Manage Jobs
+      </Link>) : (
       <Link
         href={"/user/profile"}
         className="block w-full py-2 px-2 mb-2 text-sm text-gray-800 hover:bg-gray-200 transition duration-300"
       >
         Profile
       </Link>
+      )}
     </div>
   );
 }
